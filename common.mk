e57996a15a49708b63a14f7e36fa7f0d2b229286
logcat7 <logcat7@users.noreply.github.com>
2015-07-13 06:14:18 +0100

    Change UBOOT_TOOLCHAIN_URL and LINUX_TOOLCHAIN_URL to HTTPS sources

--- common.mk
+++ common.mk
@@ -19,7 +19,7 @@ RAMDISK_FILE := uInitrd
 IMAGE_FILE := sdcard-$(DIST).img
 
 UBOOT_TOOLCHAIN := gcc-linaro-arm-none-eabi-4.8-2014.04_linux.tar.xz
-UBOOT_TOOLCHAIN_URL := http://releases.linaro.org/14.04/components/toolchain/binaries/$(UBOOT_TOOLCHAIN)
+UBOOT_TOOLCHAIN_URL := https://releases.linaro.org/14.04/components/toolchain/binaries/$(UBOOT_TOOLCHAIN)
 UBOOT_TC_DIR := uboot_tc
 UBOOT_TC_PATH := $(UBOOT_TC_DIR)/gcc-linaro-arm-none-eabi-4.8-2014.04_linux/bin
 UBOOT_REPO := https://github.com/hardkernel/u-boot.git
@@ -27,7 +27,7 @@ UBOOT_BRANCH := odroidc-v2011.03
 UBOOT_SRC := u-boot
 
 LINUX_TOOLCHAIN := gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
-LINUX_TOOLCHAIN_URL := http://releases.linaro.org/14.09/components/toolchain/binaries/$(LINUX_TOOLCHAIN)
+LINUX_TOOLCHAIN_URL := https://releases.linaro.org/14.09/components/toolchain/binaries/$(LINUX_TOOLCHAIN)
 LINUX_TC_DIR := linux_tc
 LINUX_TC_PATH := $(LINUX_TC_DIR)/bin
 LINUX_TC_PREFIX := arm-linux-gnueabihf-
